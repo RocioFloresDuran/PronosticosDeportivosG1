b2b9bdc287950d6dcb72587fba9d5f3a7cda403c
RocioFloresDuran <rochy.dmf@gmail.com>
2023-03-26 22:01:12 -0300

    Agregado de clases del modelo

---
+++ src/modelo/Equipo.java
@@ -0,0 +1,24 @@
+package modelo;
+
+public class Equipo {
+	private String nombre;
+//Constructor
+	public Equipo(String nombre) {
+	this.nombre = nombre;
+}
+
+	public Equipo() {
+}
+	
+//Método
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+//probando comentario
+        // otra prueba
+}
