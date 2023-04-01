5aea6245152bc0908ae20ec7845d95fb412a1e63
vanesaarandavera <vanesaa715@gmail.com>
2023-04-01 15:45:11 -0300

    string de archivo resultados

--- src/modelo/GestorPronosticos.java
+++ src/modelo/GestorPronosticos.java
@@ -3,6 +3,14 @@ package modelo;
 public class GestorPronosticos {
 
 	public static void main(String[] args) {
+		String archivoResultados = "C:\Users\Vanesa\eclipse-workspace\PronosticosDeportivosG1\src\modelo\Resultados.csv";
+		String idPartido = "";
+		Equipo equipo1 = new Equipo();
+		Equipo equipo2 = new Equipo();
+		int goles1 = 0;
+		int goles2 = 0;
+						
+				
 
 	}
 
